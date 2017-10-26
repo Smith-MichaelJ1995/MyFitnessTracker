f1fa95a69a0f936f1e4ccab90011c1d3236f3c00
Smith-MichaelJ1995 <michaelsmith1388@yahoo.com>
2017-10-25 22:07:34 -0400

    Implementation of index, header, footer.

--- jquery-mockup/src/navigation.ts
+++ jquery-mockup/src/navigation.ts
@@ -2,19 +2,17 @@ import * as $ from 'jquery';
 declare const navProperties: any;
 
 $(function(){
-    $("#header-placeholder").load("_header.html", function () {
-        /*
-        $("#nav-placeholder").load(navProperties.mainNav, function(){
-            $(".nav-link").removeClass("active");
-            $(`#${navProperties.currentSection}-link`).addClass("active");           
-        });       
-        */
-    } );
-    
+    /*Load the page components */
+    $("#header-placeholder").load("_header.html", function () {} );
 
+    $("#footer-placeholder").load("_footer.html", function () {} );
+
+
+    
+    /*
     $(".btn-primary").click(function(){
         $(this).closest(".col-md-4").css({ 'max-width': 'none'})
             .animate({ 'flex-basis' : '100%' })
     })
-
+    */
 });
