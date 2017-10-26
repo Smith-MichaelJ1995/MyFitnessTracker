55430d3429a5a80d3a9c6e905250381626c6d716
Smith-MichaelJ1995 <michaelsmith1388@yahoo.com>
2017-10-25 20:28:31 -0400

    initial css styling

--- jquery-mockup/src/navigation.ts
+++ jquery-mockup/src/navigation.ts
@@ -3,10 +3,12 @@ declare const navProperties: any;
 
 $(function(){
     $("#header-placeholder").load("_header.html", function () {
+        /*
         $("#nav-placeholder").load(navProperties.mainNav, function(){
             $(".nav-link").removeClass("active");
             $(`#${navProperties.currentSection}-link`).addClass("active");           
         });       
+        */
     } );
     
 
